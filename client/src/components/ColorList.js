1e49b9f47e7acbcc8edb7e0737c8b8498f862507
Tony Walker <Walkertony509@gmail.com>
2020-05-01 20:54:32 -0700

    I have completed MVP.

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -16,16 +16,32 @@ const ColorList = ({ colors, updateColors }) => {
     setColorToEdit(color);
   };
 
+  const setUpdatedColors = () => {
+    axiosWithAuth()
+      .get("/colors")
+      .then(res => {
+        updateColors(res.data);
+      })
+      .catch(err => console.log(err.message));
+  };
+
   const saveEdit = e => {
     e.preventDefault();
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
+    axiosWithAuth()
+      .put(`colors/${colorToEdit.id}`, colorToEdit)
+      .then(() => {
+        setUpdatedColors();
+      })
+      .catch(err => console.log(err.message));
   };
 
   const deleteColor = color => {
-    // make a delete request to delete this color
-  };
+    axiosWithAuth()
+      .delete(`/colors/${color.id}`)
+      .then(() => {
+        setUpdatedColors();
+      });  
+    };
 
   return (
     <div className="colors-wrap">
@@ -34,12 +50,14 @@ const ColorList = ({ colors, updateColors }) => {
         {colors.map(color => (
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
-              <span className="delete" onClick={e => {
-                    e.stopPropagation();
-                    deleteColor(color)
-                  }
-                }>
-                  x
+            <span
+                className="delete"
+                onClick={e => {
+                  e.stopPropagation();
+                  deleteColor(color);
+                }}
+              >
+                x
               </span>{" "}
               {color.color}
             </span>
